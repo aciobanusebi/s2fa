9c8fd28f83552cac621cab80657cf5a158c6b0ea
aciobanusebi <aciobanusebi@gmail.com>
2019-12-09 15:18:17 +0200

    Add files via upload

--- R/mS3faFitIteration.R
+++ R/mS3faFitIteration.R
@@ -1,57 +1,53 @@
 mS3faFitIteration <- function(trainInput, trainOutput, params, type="fa", withCorrection=FALSE, lambdaRidge=lambdaRidge) {
   nTrain <- nrow(trainInput)
-  
+
   E <- mS3faFitE(trainInput,trainOutput,params)
   E_x <- E$E_x
   E_xx <- E$E_xx
   E_z <- E$E_z
   E_zz <- E$E_zz
   E_xz <- E$E_xz
-  
-  mu_z <- 0
+
+  E_xx_sum <- 0
+  E_x_sum <- 0
+  E_zz_sum <- 0
+  E_z_sum <- 0
+  E_xz_sum <- 0
   for(i in 1:nTrain) {
-    mu_z <- mu_z + E_z[[i]]
+    E_xx_sum <- E_xx_sum + E_xx[[i]]
+    E_x_sum <- E_x_sum + E_x[[i]]
+    E_zz_sum <- E_zz_sum + E_zz[[i]]
+    E_z_sum <- E_z_sum + E_z[[i]]
+    E_xz_sum <- E_xz_sum + E_xz[[i]]
   }
-  mu_z <- mu_z / nTrain
-  
-  Sigma_z <- 0
-  for(i in 1:nTrain) {
-    Sigma_z <- Sigma_z + E_zz[[i]] - E_z[[i]] %*% t(mu_z) - mu_z %*% t(E_z[[i]]) + mu_z %*% t(mu_z)
-  }
+
+  mu_z <- E_z_sum / nTrain
+
+  Sigma_z <- E_zz_sum - E_z_sum %*% t(mu_z) - mu_z %*% t(E_z_sum) + nTrain*mu_z %*% t(mu_z)
+
+
   if(withCorrection) {
     Sigma_z <- Sigma_z / (nTrain - 1)
   } else {
     Sigma_z <- Sigma_z / nTrain
   }
-  
-  mu_x <- 0
-  for(i in 1:nTrain) {
-    mu_x <- mu_x + E_x[[i]]
-  }
-  mu_x <- mu_x/nTrain
-  
-  lambdaLeft <- 0
-  lambdaRight <- 0
-  for(i in 1:nTrain) {
-    lambdaLeft <- lambdaLeft + E_xz[[i]]
-    lambdaRight <- lambdaRight + E_zz[[i]]
-  }
-  lambdaRight <- lambdaRight + lambdaRidge * diag(ncol(trainOutput))
-  lambda <- (nTrain * mu_x %*% t(mu_z) - lambdaLeft) %*% solve(nTrain * mu_z %*% t(mu_z) - lambdaRight)
-  
+
+  mu_x <- E_x_sum/nTrain
+
+  lambdaRight <- E_zz_sum + lambdaRidge * diag(ncol(trainOutput))
+  lambda <- (nTrain * mu_x %*% t(mu_z) - E_xz_sum) %*% solve(nTrain * mu_z %*% t(mu_z) - lambdaRight)
+
   mu <- mu_x - lambda %*% mu_z
-  
-  psi <- 0
-  for(i in 1:nTrain) {
-    firstTerm <- E_xx[[i]] - E_x[[i]] %*% t(mu) - mu %*% t(E_x[[i]]) + mu %*% t(mu)
-    secondTerm <- E_xz[[i]] %*% t(lambda) - mu %*% t(E_z[[i]]) %*% t(lambda)
-    thirdTerm <- t(secondTerm)
-    fourthTerm <- lambda %*% E_zz[[i]] %*% t(lambda)
-    
-    psi <- psi + firstTerm - secondTerm - thirdTerm + fourthTerm
-  }
+
+  firstTerm <- E_xx_sum - E_x_sum %*% t(mu) - mu %*% t(E_x_sum) + nTrain*mu %*% t(mu)
+  secondTerm <- E_xz_sum %*% t(lambda) - mu %*% t(E_z_sum) %*% t(lambda)
+  thirdTerm <- t(secondTerm)
+  fourthTerm <- lambda %*% E_zz_sum %*% t(lambda)
+
+  psi <- firstTerm - secondTerm - thirdTerm + fourthTerm
+
   psi <- psi / nTrain
-  
+
   if(type == "fa") {
     if(length(psi) != 1) {
       psi <- diag(diag(psi))
@@ -61,7 +57,9 @@ mS3faFitIteration <- function(trainInput, trainOutput, params, type="fa", withCo
     nu <- sum(diag(psi)) / nDimTrain
     psi <- diag(nDimTrain) * nu
   }
-  
+
+  print("LOCAL")
+
   list(
     mu_z=mu_z,
     Sigma_z=Sigma_z,
